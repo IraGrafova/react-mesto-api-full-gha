9141e07124114511dac328be06ff34f0decf5e78
IraGrafova <i.grafova@ntc-hitech.ru>
2023-07-09 13:01:53 +0300

    env

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,9 +4,9 @@ const { LoginError } = require('./errors');
 const auth = (req, res, next) => {
   const token = req.cookies.jwt;
   let payload;
-
+  const { NODE_ENV, JWT_SECRET } = process.env;
   try {
-    payload = jwt.verify(token, process.env.JWT_SECRET);
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     throw (new LoginError('Отсутствуют права для данного действия'));
   }
